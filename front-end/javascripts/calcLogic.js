97cccac01fe1ddd02ba70e3868436b8403f2cfcd
Nicholas Castaneda <npcastaneda@gmail.com>
2016-02-15 15:27:03 -0800

    can solve two step equations, but errors in some cases

--- front-end/javascripts/calcLogic.js
+++ front-end/javascripts/calcLogic.js
@@ -12,7 +12,7 @@
   // Expression Level: [term 1] or [term 1, plus or minus, term 2]
   // Term Level: [Bool for sign, [int, divisor], Bool for var, [var?, var] ]
   // Example below: 5x + -3 = -18
-  var exampleEquation = [ [ [false, [3, 1], [false] ] ], "=", [ [false, [18, 1], [false] ] ] ]
+  var exampleEquation = [ [ [true, [7, 1], [true, "x"] ], "+", [false, [3, 1], [false] ] ], "=", [ [false, [18, 1], [false] ], "+", [true, [2, 1], [true, "x"] ] ] ]
 
 ////////////////////////////////////////////////////////////////////////
 //                      Convert to Data Structure                     //
@@ -36,7 +36,8 @@
   }
 
   // Takes an expression and converts it to an array of its terms
-  // Split the expression at all the spaces.
+  // Split the expression at all the spaces and declare an termArray.
+  // Run each term in the splitTerm funcion and push it to the termArray
   function splitExpression(expression){
     var expressionArray = expression.split(' ');
     var expressionTermArray = []
@@ -48,21 +49,29 @@
     return expressionTermArray
   }
 
-  ///// Make sure changes double negative, and is smart about fractions
+  ///// Make sure it's smart about fractions
   // Takes a term and converts it to an array of its attributes
   function splitTerm(term){
+    // First trim the whitespace and start with the termArr template
+    // If the input is an operator, return it, instead of a term
     term = term.trim()
     var termArr = [true, [0, 1], [false] ];
     if (term == '+' || term == '-') return term;
+    // If the first character of the input is "-", trim it from the
+    // input and make the positive attribute of termArr false.
     if(term[0] == '-'){
       termArr[0] = false;
       term = term.substr(1);
     }
+    // If there's a variable, edit the termArr appropriately and cut the
+    // var from the input
     if (/[a-zA-Z]/.test( term[term.length - 1] ) ){
       termArr[2][0] = true;
       termArr[2].push(term[term.length - 1]);
       term = term.substring(0, term.length - 1);
     }
+    // All that's left in the string is the number, so make it a js
+    // number and edit the termArr template appropriately
     if(term.length > 0) termArr[1][0] = Number(term);
     return termArr;
   }
@@ -71,38 +80,56 @@
 //                           Print Equation                           //
 ////////////////////////////////////////////////////////////////////////
 
-  //
+  // Takes an equation data structure and converts it to an array of
+  // term strings
   function printEquation(equation){
     var printArray = ['','','','=','','',''];
-    // First Side
+    // If the side of the equation has only one term, push a "plus" and
+    // a zero to the side
     if(equation[0].length == 1){
       equation[0].push("+");
       equation[0].push([true, [0, 1], [false] ]);
     }
+    // If both terms are zero, put a zero in the third collumn
     if(equation[0][0][1][0] == 0 && equation[0][2][1][0] == 0) {
       printArray[2] = "0";
     }
+    // If only the second term is zero, print the first term to the
+    // third collumn
     else if(equation[0][2][1][0] == 0) printArray[2] = printTerm(equation[0][0]);
+    // Otherwise print the second term in the first collumn. If the
+    // first term is not zero, print the first in the first collumn and
+    // the operation in the second collumn.
     else{
       printArray[2] = printTerm(equation[0][2]);
-      if( !(equation[0][2][1][0] == 0) ) printArray[0] = printTerm(equation[0][0]);
+      if( equation[0][0][1][0] !== 0 ){
+        printArray[0] = printTerm(equation[0][0]);
+        printArray[1] = equation[0][1]
+      }
     }
-    // Second Side
+    // Do the same for the second side. Maybe can refactor, but fuck it
     if(equation[2].length == 1){
       equation[2].push("+");
       equation[2].push([true, [0, 1], [false] ]);
     }
     if(equation[2][0][1][0] == 0 && equation[2][2][1][0] == 0) {
-      printArray[2] = "0";
+      printArray[4] = "0";
     }
-    else if(equation[2][2][1][0] == 0) printArray[2] = printTerm(equation[2][0]);
+    else if(equation[2][0][1][0] == 0) printArray[4] = printTerm(equation[2][2]);
     else{
-      printArray[2] = printTerm(equation[2][2]);
-      if( !(equation[2][2][1][0] == 0) ) printArray[0] = printTerm(equation[2][0]);
+      printArray[4] = printTerm(equation[2][0]);
+      if( equation[2][2][1][0] !== 0 ){
+        printArray[6] = printTerm(equation[2][2]);
+        printArray[5] = equation[2][1];
+      }
     }
     return printArray
   }
 
+  // Takes a termArray and converts it to a string
+  // If the term is negative, add "-" to the string.
+  // If theres a variable, add it to the string with its coefficient.
+  // Otherwise, just add the number to the string
   function printTerm(term){
     var termString = ""
     if ( !term[0] ) termString += "-";
@@ -119,32 +146,75 @@
 
   // This function allows you to add, subtract, divide, and multiply a
   // constant to both sides of a equation
-  //
-  function hitBothSides(equation, operation, operator){
-    if( !/[a-zA-Z]/.test(operator) ){
-      for(i=0; i<equation.length; i+=2){
-        for(j=0; j<equation[i].length; j+=2) { // Only works if terms combined
-          if(!equation[i][j][2][0]){
-            if (operation == "+" && equation[i][j][0]) equation[i][j][1][0] = equation[i][j][1][0] + operator;
-            else if (operation == "-" && equation[i][j][0]) equation[i][j][1][0] = equation[i][j][1][0] - operator;
-            else if (operation == "+" && !equation[i][j][0]){
-              equation[i][j][1][0] = (equation[i][j][1][0] * -1) + operator;
-              equation[i][j][0] = true;
-            } else if (operation == "-" && !equation[i][j][0]){
-              equation[i][j][1][0] = (equation[i][j][1][0] * -1) - operator;
-              equation[i][j][0] = true;
-            } else if (operation == "*"){
-
-            } else if (operation == "/"){
-
-            }
+  // HitBothSides takes, in an equation, an operator, and a operand.
+  // Loop through the terms in each side of the equation
+  function hitBothSides(equation, operator, operand){
+    for(i=0; i<equation.length; i+=2){
+      for(j=0; j<equation[i].length; j+=2) {
+        // If both the operand and the terms are constants or if both
+        // the operand and the operator have variables, first change the
+        // operand to just the coefficient, and then ...
+        // FIX for "x" as operand
+        if( (!equation[i][j][2][0] && !/[a-zA-Z]/.test(operand) && equation[i][j][1][0] !== 0) || (equation[i][j][2][0] && /[a-zA-Z]/.test(operand) && equation[i][j][1][0] !== 0)) {
+          var formatedOperand = /[a-zA-Z]/.test(operand) ? Number(operand.substring(0, operand.length - 1)) : formatedOperand = operand;
+          // NEED TO BE FIXED and then commented
+          if (operator == "+" && equation[i][j][0]) equation[i][j][1][0] = equation[i][j][1][0] + formatedOperand;
+          else if (operator == "-" && equation[i][j][0]) equation[i][j][1][0] = equation[i][j][1][0] - formatedOperand;
+          else if (operator == "+" && !equation[i][j][0]){
+            equation[i][j][1][0] = (equation[i][j][1][0] * -1) + formatedOperand;
+            equation[i][j][0] = true;
+          } else if (operator == "-" && !equation[i][j][0]){
+            equation[i][j][1][0] = (equation[i][j][1][0] * -1) - formatedOperand;
+            equation[i][j][0] = true;
           }
+        }
+        // NEED TO BE FIXED doesn't wor
+        else if (!/[a-zA-Z]/.test(operand) && equation[i][0][1][0] == 0 && equation[i][0][1][0] == 0){
+          if(i == 0) equation[i][2] = [true, [Number(operand), 1] [false] ];
+          if(i == 2) equation[i][0] = [true, [Number(operand), 1] [false] ];
+        }
+        // If you're multiplying or dividing both sides, multiply or
+        // divide each term by the operand.
+        // Error if you divide by zero
+        if( !/[a-zA-Z]/.test(operand) ){
+          if(operator == "*") equation[i][j][1][0] *= operand;
+          if(operator == "/"){
+            if (operand == 0) return equation; // flash an error eventually
+            else equation[i][j][1][0] /= operand;
+          }
+        } else if ( /[a-zA-Z]/.test(operand) && ( operator == "*" || operator == "/" ) ){
+          return equation; // flash an error eventually
         }
       }
     }
     return equation;
   }
 
+  // Insert function to check if solved
 
+////////////////////////////////////////////////////////////////////////
+//                          Tests and Notes                           //
+////////////////////////////////////////////////////////////////////////
+
+console.log(printEquation(exampleEquation));
+console.log("Subtracting 2x from both sides");
+exampleEquation = hitBothSides(exampleEquation, "-", "2x");
+console.log(printEquation(exampleEquation));
+console.log("Adding 5 to both sides");
+exampleEquation = hitBothSides(exampleEquation, "+", 5)
+console.log(printEquation(exampleEquation));
+console.log("Subtracting 2 from both sides");
+exampleEquation = hitBothSides(exampleEquation, "-", 2)
+console.log(printEquation(exampleEquation));
+console.log("Multiplying both sides by 2");
+exampleEquation = hitBothSides(exampleEquation, "*", 2)
+console.log(printEquation(exampleEquation));
+console.log("Dividing both sides by 10");
+exampleEquation = hitBothSides(exampleEquation, "/", 10)
+console.log(printEquation(exampleEquation));
+
+// Need to deal with fractions and decimals
+// Some Issues with negatives
+// What about no solution?
 
 // })
