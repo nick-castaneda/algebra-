7a74ba3675198b444eee63327a4ec8529bf1ce94
Nicholas Castaneda <npcastaneda@gmail.com>
2016-02-15 17:23:57 -0800

    Home and 3 more module pages

--- front-end/javascripts/server.js
+++ front-end/javascripts/server.js
@@ -5,7 +5,23 @@ function MainRouter($stateProvider, $urlRouterProvider){
   $stateProvider
     .state('home', {
       url: "/",
+      templateUrl: "partials/home.html"
+    })
+    .state('equation-solver', {
+      url: "/",
       templateUrl: "partials/equation-solver.html"
+    })
+    .state('expression-simplifier', {
+      url: "/",
+      templateUrl: "partials/expression-simplifier.html"
+    })
+    .state('sat-problems', {
+      url: "/",
+      templateUrl: "partials/sat-problems.html"
+    })
+    .state('notes', {
+      url: "/",
+      templateUrl: "partials/notes.html"
     })
 
   $urlRouterProvider.otherwise("/");
